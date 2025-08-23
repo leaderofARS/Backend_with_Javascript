ed32ca68622b2e053c32726a59321ed9dbd671b7
leaderofARS <abhayrshanbhag@hotmail.com>
2025-08-23 12:52:49 +0530

    JustFileOrganization

---
+++ JustFileOrganization/src/utils/mathUtils.js
@@ -0,0 +1,32 @@
+export function add(a, b){
+    return a+b;
+}
+export function sub(a, b){
+    return a-b;
+}
+export function mul(a, b){
+    return a*b;
+}
+export function div(a, b){
+    return a/b;
+}
+export function mod(a, b){
+    return a%b;
+}
+export function square(a){
+    return a*a;
+}   
+export function cube(a){
+    return a*a*a;
+}  
+export function sqrt(a){
+    return Math.sqrt(a); 
+}
+export function power(a, b){
+    return Math.pow(a, b);
+}
+export const PI = 3.14159;
+export const E = 2.71828;
+export const GOLDEN_RATIO = 1.61803;
+export const SQRT2 = 1.41421;
+export const SQRT3 = 1.73205;
